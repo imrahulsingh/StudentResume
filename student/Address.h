2c2bfcc68f768f8f415598df1beb76c9a303beca
Rahul Roy <45142775+imrahulsingh@users.noreply.github.com>
2022-09-14 17:36:02 +0530

    Add files via upload

---
+++ student/Address.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<string>
+
+using namespace std;
+
+class Address
+{
+public:
+    string currAddress;
+    string city;
+    string taluka;
+    string district;
+    int pincode;
+};
+
